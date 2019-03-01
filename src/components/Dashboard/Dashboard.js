d30e6a2a3f0f6878fbcb4ea2dfbdd841954e3a23
Samantha Jeet <samanthajeet@gmail.com>
2019-03-01 14:07:32 -0700

    part 2 step 2

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import Post from '../Post/Post'
 
 class Dahsboard extends Component {
-  state = {  }
+  constructor(props){
+    super(props)
+    this.state= {
+      myPosts: true,
+      chkbox: true,
+      posts: [{postTitle: 'Title', username: 'username', user_image: 'img', message: 'message1'},
+      {postTitle: 'Title', username: 'username', user_image: 'img', message: 'message2'}]
+    }
+  }
+
+  handleMyPosts = () =>{
+    this.setState({
+      myPosts: !this.state.myPosts
+    })
+  }
+
   render() { 
+    const mappedPosts = this.state.posts.map( post => {
+      return (
+        <Post
+          key={post.post}
+          title={post.title}
+          username={post.username}
+          message={post.message}
+        />
+      )
+    })
+    console.log(this.state)
     return ( 
       <div>
         <h1>Dashboard Component</h1>
+        <input 
+          placeholder="search"
+        />
+
+        <button>Search</button>
+        <button>Reset</button>
+        <input 
+          type="checkbox"
+          value={true}
+          defaultChecked={this.state.chkbox}
+          onChange={this.handleMyPosts}
+          
+        /> MyPosts
+
+        {mappedPosts}
+
       </div>
      );
   }
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,11 +3,14 @@ import React, { Component }  from 'react'
 
 
 class Post extends Component {
-  state = {  }
+  
   render() { 
     return ( 
       <div>
         <h1>Post Component</h1>
+        <h2>{this.props.title}</h2>
+        <h5>{this.props.username}</h5>
+        <p>{this.props.message}</p>
       </div>
      );
   }
