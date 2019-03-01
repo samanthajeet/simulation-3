8805a74c60132c50c062b34a9e7d171150e71ce6
Samantha Jeet <samanthajeet@gmail.com>
2019-03-01 10:42:37 -0700

    React set-up

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Dahsboard extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        <h1>Dashboard Component</h1>
+      </div>
+     );
+  }
+}
+ 
+export default Dahsboard;
---
+++ src/components/Post/Post.js
@@ -0,0 +1,16 @@
+import React, { Component }  from 'react'
+
+
+
+class Post extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        <h1>Post Component</h1>
+      </div>
+     );
+  }
+}
+ 
+export default Post;
